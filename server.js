c25c509855d712da4745039ea6b13fcf959370c9
Eduardo Santillana <edosantillana@gmail.com>
2017-05-03 17:57:22 +0200

    new commit

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 app.get('/', function(request, response) {
   response.render('public/index');
+});
+
+app.listen(port, function(){
+  console.log('App running!');
 });
 
 app.get('/:dateValue', sendDate);
